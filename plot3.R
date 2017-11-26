aeb724ee6dbd010c4ea021cbf85f537cfcad032c
AlfredoArandan <32247734+AlfredoArandan@users.noreply.github.com>
2017-11-26 17:57:19 -0300

    plot3.R

---
+++ plot3.R
@@ -0,0 +1,13 @@
+#Plot Number 3
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+ESM1<-as.numeric(subSetData$Sub_metering_1)
+ESM2<-as.numeric(subSetData$Sub_metering_2)
+ESM3<-as.numeric(subSetData$Sub_metering_3)
+with(subSetData,plot(datetime,ESM1,type = "n", ylab="Energy Submetering", xlab = ""))
+with(subSetData,points(datetime,ESM1,type="l", col = "black"))
+with(subSetData,points(datetime,ESM2,type="l", col = "red"))
+with(subSetData,points(datetime,ESM3,type="l", col = "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.copy(png,file = "plot3.png")
+dev.off()
