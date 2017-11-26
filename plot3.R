4054cb5f3ecec7771a0ed6d766efd586e37eec4e
AlfredoArandan <32247734+AlfredoArandan@users.noreply.github.com>
2017-11-26 17:57:55 -0300

    plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 #Plot Number 3
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 ESM1<-as.numeric(subSetData$Sub_metering_1)
